61e701dd792dabc758abeb04bb12b89d09e5d141
James Coen <jcoen44@hotmail.com>
2020-08-14 18:28:43 +0100

    style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { loadHome } from './loadHome'
 import { navLinks } from './navLinks'
 import { loadBurger } from './burger'
-import "./styles.css";
+import "./src/styles.css";
 
 
 loadHome();
